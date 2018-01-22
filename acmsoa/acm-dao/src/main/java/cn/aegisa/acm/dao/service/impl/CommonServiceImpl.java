0c6a4b23562db450fbc6c0d133db84377c0c9bad
缐英达 <daegis@yeah.net>
2018-01-22 15:08:11 +0800

    1

--- acmsoa/acm-dao/src/main/java/cn/aegisa/acm/dao/service/impl/CommonServiceImpl.java
+++ acmsoa/acm-dao/src/main/java/cn/aegisa/acm/dao/service/impl/CommonServiceImpl.java
@@ -5,18 +5,19 @@ import cn.aegisa.acm.dao.spi.ICommonDao;
 /**
  * Created by simple on 2016/11/11.
  */
-public class CommonServiceImpl extends  DefaultServiceImpl {
+public class CommonServiceImpl extends DefaultServiceImpl {
 
     private ICommonDao commonDao;
+
     @Override
     public ICommonDao getCommonDao() {
         return commonDao;
     }
-    @Override
-    public   void setCommonDao( ICommonDao commonDao){
-        this.commonDao =commonDao;
-    }
 
+    @Override
+    public void setCommonDao(ICommonDao commonDao) {
+        this.commonDao = commonDao;
+    }
 
 
 }
